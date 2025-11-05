ac00dc118209449c42f8b56e48de0d5a05f47dcf
Bento Eno <aabid6000@gmail.com>
2025-11-05 15:50:32 +0900

    solve 06 repeat string

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+// create function that takes two argument
+// initialize the string 
+let repeated = '';
+if (num < 0) {
+    return 'ERROR'
+} else {
+  for (let index = 1; index <= num; index++) {
+// concat with additional string based on how many its repeated
+    repeated = repeated + string;  
+}
+}
+return repeated
 };
 
 // Do not edit below this line
