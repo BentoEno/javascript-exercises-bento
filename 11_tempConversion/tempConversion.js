86b5558939145961a10a7d8de24cd6b19114c758
Bento Eno <aabid6000@gmail.com>
2025-11-05 19:25:16 +0900

    solve 11 tempt conversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degree) {
+  const celcius = (degree - 32) * 5 / 9;
+  return (Math.round(celcius * 10))/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degree) {
+  const fahren = (degree * 9 / 5) + 32;
+  return (Math.round(fahren * 10))/10;
+
 };
 
 // Do not edit below this line
