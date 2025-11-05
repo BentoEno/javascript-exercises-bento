a175e1b768aecbe5e1189420826b41a765b04d27
Bento Eno <aabid6000@gmail.com>
2025-11-05 16:06:13 +0900

    solve 07 reverse string

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const charArr = [...string].reverse();
+    const inverse = charArr.join('');
+    return inverse;
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
+// // reverse the position of the string
+// let newString = '';
+// for (let index = 0; index < string.length; index++) {
+//     let inverseIndex = index * -1;
+//     newString[inverseIndex] = string[index];
+// }
+// // return it
+// return newString
