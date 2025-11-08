86208afae5fad9673192e462d1c0dca76c43fc21
Bento Eno <aabid6000@gmail.com>
2025-11-08 23:37:03 +0900

    solve 12 calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(...numbers) {
+  return numbers.reduce((sum, currentValue) => sum + currentValue)
+}
+
+const subtract = function(...numbers) {
+  return numbers.reduce((sum, currentValue) => sum - currentValue)
 };
 
-const subtract = function() {
-	
+const sum = function(numbers) {
+  if (numbers.length === 0) {
+    return 0;
+  }
+	return numbers.reduce((sum, currentValue) => sum + currentValue)
 };
 
-const sum = function() {
-	
+const multiply = function(numbers) {
+  return numbers.reduce((sum, currentValue) => sum * currentValue)
 };
 
-const multiply = function() {
-
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(number) {
+  let sum = 1;
+	for (let index = 1; index <= number; index++) {
+    sum *= index;
+  }
+  return sum;
 };
 
 // Do not edit below this line
