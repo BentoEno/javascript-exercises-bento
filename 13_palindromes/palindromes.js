5a273d9e8184378385e8081e60d267477a9d662e
Bento Eno <aabid6000@gmail.com>
2025-11-09 11:40:18 +0900

    solve 13 palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (rawText) {
+    const cleanText = rawText.replace(/[^a-zA-Z0-9]/g, "");
+    const text = cleanText.toLowerCase();
+    for (let i = 0; i < text.length; i++) {
+        const start = text[i]
+        const last = text[text.length - 1 - i]
+        const middleIndex = Math.floor(text.length/2)
+        if (start !== text[middleIndex] && start !== last) {
+            return false
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
