21434d1360bf8fd48508fb2154c94d69917ddc6d
Bento Eno <aabid6000@gmail.com>
2025-11-09 14:16:21 +0900

    solve 16 find the oldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    arr.sort((a, b) => {
+        if (!a.yearOfDeath) {
+            a.yearOfDeath = new Date().getFullYear();
+        } else if (!b.yearOfDeath) {
+            b.yearOfDeath = new Date().getFullYear();
+        }
 
+        const lifeA = a.yearOfDeath - a.yearOfBirth;
+        const lifeB = b.yearOfDeath - b.yearOfBirth;
+
+        if (lifeA > 0 && lifeB > 0) {
+            return lifeB - lifeA;
+        }
+    })
+
+    return arr[0];
 };
 
 // Do not edit below this line
