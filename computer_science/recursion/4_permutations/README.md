b6b831c0b3f7248c1379caaa313f4b80ffb61c4c
ahnafabid <aabid6000@gmail.com>
2026-06-03 10:19:38 +0900

    permutation solved

--- computer_science/recursion/4_permutations/README.md
+++ computer_science/recursion/4_permutations/README.md
@@ -1,4 +1,4 @@
-# Exercise 4 - permutations
+ # Exercise 4 - permutations
 
 Write a function that takes in an empty array or an input array of consecutive positive integers, starting at 1, and returns an array of all possible permutations of the original array.
 
--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -1,5 +1,19 @@
-const permutations = function() {
-  
+const permutations = function(arr) {
+  const result = [];
+  if (arr.length === 0) return [[]];
+  if (arr.length === 1) return [arr];
+
+  for (let i = 0; i < arr.length; i++) {
+    const currentNum = arr[i];
+    const remainingNum = arr.slice(0, i).concat(arr.slice(1 + i))
+    
+    const remainingPermutedArr = permutations(remainingNum);
+    for (let j = 0; j < remainingPermutedArr.length; j++) {
+        const permuttedArr = [currentNum].concat(remainingPermutedArr[j])
+        result.push(permuttedArr)
+    }
+  }
+  return result
 };
   
 // Do not edit below this line
